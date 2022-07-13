4b52bc6c46297240e1ef5d4b2db699a49353cc01
sinawic <sina_041_sm@yahoo.com>
2022-07-13 21:06:21 +0430

    strategy basics added

---
+++ src/auth/Strategy/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../auth.service';
+import {
+  ExtractJwt,
+  Strategy,
+} from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+  'jwt',
+) {
+  constructor(
+    private authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.ACCESS_TOKEN_SECRET,
+    });
+  }
+
+  async validate({ _id, room }) {
+    const user = await this.authService.getRequesterAdvertiser({ _id, room })
+    return user;
+  }
+}
